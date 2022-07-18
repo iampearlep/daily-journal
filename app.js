da89f6823897def97dd23b1535b7c5cbae369074
iampearlep <damilolaprincess@gmail.com>
2022-07-18 19:47:55 +0100

    Add mongoose, modified home.ejs and app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
 const { redirect } = require("express/lib/response");
 const _ = require("lodash");
 const { lowerCase } = require("lodash");
@@ -18,14 +19,65 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-let posts = [];
+mongoose.connect("mongodb://localhost:27017/blogDB", {useNewUrlParser: true});
+
+
+const postSchema = {
+  title: String,
+  content: String
+};
+
+const Post = mongoose.model("Post", postSchema);
+
+
+
 
 app.get("/", function(req, res) {
-  res.render("home", {
-    homeStartingContent: homeStartingContent,
-    posts: posts
+  Post.find({}, function(err, posts){
+    res.render("home", {
+      homeStartingContent: homeStartingContent,
+      posts: posts
+      });
   });
 });
+
+
+app.get("/compose", function(req, res) {
+  res.render("compose");
+
+});
+app.post("/compose", function(req, res) {
+  const post = new Post ({
+    title: req.body.postTitle,
+    content: req.body.postBody
+  });
+
+
+  post.save(function(err){
+
+    if (!err){
+ 
+      res.redirect("/");
+ 
+    }
+ 
+  });
+
+})
+
+
+app.get("/posts/:postId", function(req, res){
+
+  const requestedPostId = req.params.postId;
+  
+    Post.findOne({_id: requestedPostId}, function(err, post){
+      res.render("post", {
+        title: post.title,
+        content: post.content
+      });
+    });
+  
+  });
 
 app.get("/about", function(req, res) {
   res.render("about", {
@@ -40,41 +92,6 @@ app.get("/contact", function(req, res) {
   });
 
 });
-
-app.get("/compose", function(req, res) {
-  res.render("compose");
-
-});
-app.post("/compose", function(req, res) {
-  const post = {
-    title: req.body.postTitle,
-    content: req.body.postBody
-  };
-posts.push(post);
-res.redirect("/");
-
-
-
-})
-
-
-app.get("/posts/:postName", function(req, res) {
-
-  const requestedTitle = _.kebabCase(req.params.postName);
-
-  posts.forEach(function(post) {
-   const storedTitle =  _.kebabCase(post.title);
-
-   if (storedTitle === requestedTitle) {
-    res.render("post", {
-      title: post.title,
-      content: post.content
-    });
-  }
-  });
- 
-});
-
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
